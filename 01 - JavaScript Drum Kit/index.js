e2a1ea8cee525c51f89d67ee818a3db159642ec1
Nichele Dunn <the7yearitch@hotmail.com>
2021-09-06 18:17:14 -0600

    📝 Update README notes and organize function playSound

--- 01 - JavaScript Drum Kit/index.js
+++ 01 - JavaScript Drum Kit/index.js
@@ -1,16 +1,14 @@
 window.addEventListener('keydown', playSound);
 
 function playSound(e) {
-  // console.log('event keycode <>>>', e.keycode);
-const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-// this will dynamically return an audio element if it has a dataKey matching the event keyCode
-// console.log("audio <>>>", audio);
-if(!audio) return;
-// stops the function from running at all
-audio.currentTime = 0;
-audio.play();
-// this will only play the audio once through -- can't instantly press the button again; need to add the currentTime resetter to allow multiple keystrikes without waiting for the play to complete
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  if(!audio) return;
+  // stops the function from running at all
+  audio.currentTime = 0;
+  // resets the audio file start time to the beginning
+  audio.play();
+  // this will only play the audio once through -- can't instantly press the button again; need to add the currentTime resetter to allow multiple keystrikes without waiting for the play to complete
   key.classList.add("playing");
 }
 
