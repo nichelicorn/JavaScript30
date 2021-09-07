7fab703adfb3962304f11d965d01858b53b3b4db
Nichele Dunn <the7yearitch@hotmail.com>
2021-09-07 15:39:20 -0600

    📝 Update README notes and remove unneeded code

--- 02 - JS and CSS Clock/README.md
+++ 02 - JS and CSS Clock/README.md
@@ -13,3 +13,9 @@
 * Write functionality to remove blurry transition when minutes / seconds / hours rollover
 * Refactor
 
+#### Tidbits
+* Use (`transform`)[https://developer.mozilla.org/en-US/docs/Web/CSS/transform] CSS properties to apply animation styles to elements on the page
+  * It applies a (`<transform-function>`)[https://developer.mozilla.org/en-US/docs/Web/CSS/transform-function] that will manipulate the element within its plane on the page
+  * Multiple formulas can be passed through for a single element
+* Transition effects can be manipulated by adding a (`transition-timing-function`)[https://developer.mozilla.org/en-US/docs/Web/CSS/transition-timing-function] rule which, "in essence, lets you establish an acceleration curve so that the speed of the transition can vary over its duration."
+  * This applies a realistic appearance to moving elements, where a small recoil effect is visible
--- 02 - JS and CSS Clock/index.js
+++ 02 - JS and CSS Clock/index.js
@@ -1,6 +1,10 @@
+// 🔎 Query Selectors
 const secondHand = document.querySelector(".second-hand");
 const minuteHand = document.querySelector(".min-hand");
 const hourHand = document.querySelector(".hour-hand");
+
+// ⚙️ Functions
+setInterval(setDate, 1000);
 
 function setDate() {
   const now = new Date();
@@ -8,22 +12,21 @@ function setDate() {
   const seconds = now.getSeconds();
   const secondsDegrees = ((seconds / 60) * 360) + 90;
   if ( seconds === 0 ) {
-    console.log("transition thing");
-    console.log("seconds <>>>", seconds);
     secondHand.style.transition = "none";
-    // line 90 has an error: none is not defined -- missing quotes
   }
   secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
   
   const minutes = now.getMinutes();
   const minutesDegrees = ((minutes / 60) * 360) + 90;
+  if ( minutes === 0 ) {
+    minuteHand.style.transition = "none";
+  }
   minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
   
   const hours = now.getHours();
   const hoursDegrees = ((hours / 12) * 360) + 90;
+  if ( hours === 0 ) {
+    hourHand.style.transition = "none";
+  }
   hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
-}
-
-
-
-setInterval(setDate, 1000);
+};
