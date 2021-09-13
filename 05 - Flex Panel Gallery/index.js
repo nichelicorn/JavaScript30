2db65ac39fa6f58eb060a5b5a3c03c4c377bb317
Nichele Dunn <the7yearitch@hotmail.com>
2021-09-13 13:53:39 -0600

    ♻️  Separate code into appropriate files

---
+++ 05 - Flex Panel Gallery/index.js
@@ -0,0 +1,19 @@
+// 🔎 Query selectors
+const panels = document.querySelectorAll(".panel");
+
+//  Event listeners
+panels.forEach(panel => panel.addEventListener("click", toggleOpen));
+panels.forEach(panel => panel.addEventListener("transitionend", toggleActive));
+
+// Functions
+function toggleOpen() {
+  console.log("hello")
+  this.classList.toggle("open");
+}
+
+function toggleActive(e) {
+  console.log("e.propertyName", e.propertyName);
+  if (e.propertyName.includes("flex")) {
+    this.classList.toggle("open-active");
+  };
+}
