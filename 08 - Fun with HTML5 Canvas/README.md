1818f4539797605024e702a25682bc867b630e67
Nichele Dunn <the7yearitch@hotmail.com>
2021-09-16 14:17:04 -0600

    📝Update notes

--- 08 - Fun with HTML5 Canvas/README.md
+++ 08 - Fun with HTML5 Canvas/README.md
@@ -11,5 +11,19 @@ Create a drawing tablet in the window!
 #### What did I learn?
 * The HTML `<canvas>` element is used to create a drawing space in the window
 
+* Use `canvas.getContext()` to set the environment for 2D or 3D rendering
+  * Context is constant once it is set - "it will always return the same drawing context instance as was returned the first time the method was invoked. It is not possible to get a different drawing context object on a given canvas element." 
+  * `canvas.getContext()` is not fully supported in Safari and Internet Explorer
+
+* Style elements can be set to the context using `CanvasRenderingContext2D.method`
+  * Includes lineWidth, lineJoin, shadowBlur, shadowColor, etc...
+
+* Lines are drawn using contextual rending methods
+  * [`ctx.beginPath()`](https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/beginPath) : creates a new path for drawing by clearing the previous list of sub-paths (the XY coordinates of the previous line)
+  * [`ctx.strokeStyle = style`](https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/strokeStyle) : sets the color, gradient, or pattern to be used for each stroke / line
+  * [`ctx.moveTo()`](https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/moveTo) : begins a new sub-path at the specfied coordinates
+
 #### References
-* 
+* [`Canvas API` on MDN : ](https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API)
+* [`HTMLCanvasElement.getContext()` on MDN : ](https://developer.mozilla.org/en-US/docs/Web/API/HTMLCanvasElement/getContext)
+* [`CanvasRenderingContext2D` on MDN : ](https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D)
--- 08 - Fun with HTML5 Canvas/index.js
+++ 08 - Fun with HTML5 Canvas/index.js
@@ -1,61 +1,65 @@
+// 🔎 Query selectors
+const canvas = document.getElementById("draw");
+const main = document.querySelector("main");
+// size up canvas to fit the window
 
-  const canvas = document.getElementById("draw");
-  const main = document.querySelector("main");
-  // size up canvas to fit the window
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight - (window.innerHeight * .2);
-  
-  // need to add a context - this is where the drawing occurs; can be 2D or 3D (for video games / 3D rendering)
-  const ctx = canvas.getContext("2d");
-  ctx.strokeStyle = "BADASS";
-  // what the line does when it meets another line - look at these on MDN!
-  ctx.lineJoin = "round";
-  ctx.lineCap = "round";
-  ctx.lineWidth = 50;
-  // ctx.globalCompositeOperation = "multiply"; // this property creates an additive layering effect with the color
-
-  // this will be used to stop the drawing when keyup / click released
-  let isDrawing = false;
-  // sets the starting and ending X and Y coordinates; 0,0 coord will start in the upper left corner
-  let lastX = 0;
-  let lastY = 0;
-  let hue = 0;
-  let direction = true;
-
-  function draw(e) {
-    if (!isDrawing) return; // stops the function from running when mouse is not down
-    // console.log("event <>>>", e);
-    ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
-    ctx.beginPath();
-    // start at an XY, move to new XY and draw in between
-    ctx.moveTo(lastX, lastY); // sets the starting point
-    ctx.lineTo(e.offsetX, e.offsetY); // these values are taken from the event
-    ctx.stroke(); // actually draws the line
-    // reset the values of lastX/Y to create new lines at the starting point
-    [lastX, lastY] = [e.offsetX, e.offsetY]; // can use destructuring to reassign both values in one line
-    hue++;
-
-    if (hue >= 360) {
-      hue = 0;
-    } // resets the hue value to 0
-
-    // ctx.lineWidth++;
-    if (ctx.lineWidth >= 100 || ctx.lineWidth <= 1) {
-      direction = !direction;
-    }
-
-    if (direction) {
-      ctx.lineWidth++
-    } else {
-      ctx.lineWidth--;
-    }
-  }
-
-  canvas.addEventListener("mousedown", (e) => {
-    isDrawing = true;
-    [lastX, lastY] = [e.offsetX, e.offsetY];
+// 👂 Event listeners
+canvas.addEventListener("mousedown", (e) => {
+  isDrawing = true;
+  [lastX, lastY] = [e.offsetX, e.offsetY];
   });
+canvas.addEventListener("mousemove", draw);
+canvas.addEventListener("mouseup", () => isDrawing = false);
+canvas.addEventListener("mouseout", () => isDrawing = false);
+  
+// 📐 Set Canvas properties
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight - (window.innerHeight * .2);
+// need to add a context - this is where the drawing occurs; can be 2D or 3D (for video games / 3D rendering)
+const ctx = canvas.getContext("2d");
+// ctx.strokeStyle = "BADASS";
+// what the line does when it meets another line - look at these on MDN!
+ctx.lineJoin = "round";
+ctx.lineCap = "round";
+ctx.lineWidth = 5;
+// ctx.globalCompositeOperation = "multiply"; // this property creates an additive layering effect with the color
 
-  canvas.addEventListener("mousemove", draw);
-  canvas.addEventListener("mouseup", () => isDrawing = false);
-  canvas.addEventListener("mouseout", () => isDrawing = false);
+// 📐 Set line properties
+// this will be used to stop the drawing when keyup / click released
+let isDrawing = false;
+// sets the starting and ending X and Y coordinates; 0,0 coord will start in the upper left corner
+let lastX = 0;
+let lastY = 0;
+let hue = 0;
+let direction = true;
+
+// ⚙️ Functions
+function draw(e) {
+  if (!isDrawing) return; // prevents the function from running when mouse is not down
+  ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;   // Sets rainbow values on pen color
+  ctx.beginPath();
+  // start at an XY, move to new XY and draw in between
+  ctx.moveTo(lastX, lastY); // sets the starting point
+  ctx.lineTo(e.offsetX, e.offsetY); // these values are taken from the event
+  ctx.stroke(); // actually draws the line
+  // reset the values of lastX/Y to create new lines at the starting point
+  [lastX, lastY] = [e.offsetX, e.offsetY]; // can use destructuring to reassign both values in one line
+
+  // Set the color shift on the rainbow pen
+  hue++;
+   // resets the hue value to 0
+  if (hue >= 360) {
+  hue = 0; 
+  };
+
+  // Set line width animation
+  if (ctx.lineWidth >= 100 || ctx.lineWidth <= 1) {
+  direction = !direction;
+  };
+  if (direction) {
+  ctx.lineWidth++
+  } else {
+  ctx.lineWidth--;
+  };
+}
+
