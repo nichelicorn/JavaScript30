14133b2b56a8ff9f833647648e4c1bad14fc23c7
Nichele Dunn <the7yearitch@hotmail.com>
2021-09-17 13:35:12 -0600

    ♻️  Refactor console logs into arrow functions

--- 09 - Dev Tools Domination/README.md
+++ 09 - Dev Tools Domination/README.md
@@ -6,6 +6,7 @@ Create a drawing tablet in the window!
 
 #### Goals
 * Learn more about the dev tools in the browser
+* Create tool to display console action in the window
 
 #### What did I learn?
 * Can set a break point using the Inspector to view actions in the code :  inspect the element before action, right click > Break on > Attribute Modification
--- 09 - Dev Tools Domination/index.js
+++ 09 - Dev Tools Domination/index.js
@@ -1,113 +1,141 @@
 // 🔎 Query selectors
 const output = document.getElementById("output");
+const h1 = document.querySelector("h1");
 
-let codeToVisualize = {}
-;
+// 👂 Event listeners
+h1.addEventListener("click", makeGreen);
 
-const dogs = [{ name: 'Snickers', age: 2 }, { name: 'Hugo', age: 8 }];
+
+const codeToVisualize = {};
+
+const dogs = [
+  { name: 'Snickers', age: 2 }, 
+  { name: 'Hugo', age: 8 }
+];
 
 function makeGreen() {
   const h1 = document.querySelector('h1');
   h1.style.color = '#BADA55';
-  h1.style.fontSize = '50px';
+  // h1.style.fontSize = '50px';
   console.log("The header is green now 🐸");
 }
 
 // Regular
-console.log("hello");
-console.log(`Hello, I am a $ { JavaScript string interpolation } !`);
+const regularCLog = () => {
+  console.log("Hello, I am a regular string.");
+  console.log(`Hello, I am a $ { JavaScript string interpolation } !`);
+};
 
 // Interpolated
-console.log("Hello, I am a %s interpolation!", "✨"); 
 // not used as frequently; inserts the second string in place of the %s in the first string
+const interpolatedCLog = () => {
+  console.log("Hello, I am an %s interpolation!", "✨"); 
+};
 
 // Styled
-console.log("%c I am some great styled text", "font-family: Courier New, serif; background: turquoise");
 // %c in front of the thing you want to style, followed by a string containing the styling rules
+const styledCLog = () => {
+  console.log("%c I am some great styled text", "font-family: Courier New, serif; background: turquoise");
+};
 
-// warning!
-console.warn("👻");
+// Warning!
 // displays a warning ⚠️ message in the console with the stack trace (where it was called)
 // use warnings and errors inside of functions if these messages are needed
+const warningCLog = () => {
+  console.warn("👻");
+};
 
-// Error :|
-console.error("👻");
+// Error
 // displays an error ❗️ message in the console with the stack trace
+const errorCLog = () => {
+  console.error("👻");
+};
 
 // Info
-console.info("Geckos lack eyelids, so they lick their eyes to wet them.");
 // displays an info ℹ️ message in the console
+const infoCLog = () => {
+  console.info("Geckos lack eyelids, so they lick their eyes to wet them.");
+};
 
 // Testing
-const h1 = document.querySelector("h1");
-console.assert(h1.classList.contains("ouch"), "There is no ouch!");
-
-console.assert(2 === 2, "That is wrong!");
 // displays an error ❗️ message if the conditional fails; does nothing if the assertion is correct
+const assertCLog = () => {
+  console.assert(h1.classList.contains("ouch"), "There is no ouch!");
 
-// clearing
-// console.clear();
-// clears the console, a frustrating reality if you need those messages!!
+  console.assert(2 === 2, "That is wrong!");
+};
+
+// Clearing
+// clears the console
+const clearCLog = () => {
+  console.clear();
+};
 
 // Viewing DOM Elements
-// console.log("p <>>>", p);
-// logs <p> element as an object
-// console.dir(p);
-// logs the element and its prototype
+const logDomElement = (e) => {
+  // console.log("event <>>>", e);
+  // console.log("event target <>>>", e.target);
+  // console.log("element <>>>", element);
+  // target the event element
+  const logThis = e.target;
+  // logs element as an object
+  // console.log("logThis <>>>", logThis);
+
+  // logs the element and its prototype
+  console.log("element prototype <>>>");
+  console.dir(logThis);
+};
+
 
 // Grouping together
-dogs.forEach(dog => {
-// console.group(`${dog.name}`);
-console.groupCollapsed(`${dog.name}`)
-console.log(`This is ${dog.name}, who is ${dog.age} years old.`);
-console.groupEnd(`${dog.name}`);
-});
 // group elements of similar type
+const groupingCLog = () => {
+  dogs.forEach(dog => {
+    // log each item in the group, with style applied
+  console.group(`%c${dog.name}`, "font-family: Courier New, serif; background-color: turquoise");
+  // log each item collapsed with a dropdown
+  // console.groupCollapsed(`${dog.name}`)
+  console.log(`This is ${dog.name}, who is ${dog.age} years old.`);
+  console.groupEnd(`${dog.name}`);
+  });
+};
 
-// counting
-console.count("pickle");
-console.count("pickle");
-console.count("radish");
-console.count("pickle");
-console.count("pickle");
-console.count("radish");
-console.count("radish");
-console.count("radish");
+// Counting
 // logs the number of times that item appears
+const countCLog = () => {
+  console.count("strawberry 🍓");
+  console.count("pickle 🥒");
+  console.count("pickle 🥒");
+  console.count("strawberry 🍓");
+  console.count("pickle 🥒");
+  console.count("pickle 🥒");
+  console.count("strawberry 🍓");
+  console.count("strawberry 🍓");
+  console.count("strawberry 🍓");
+};
+  
 
-// timing
-console.time("fetching data");
-fetch("https://api.github.com/users/nichelicorn")
-  .then(data => data.json())
-  .then(data => {
-  console.timeEnd("fetching data");
-  console.log(data);
-});
+// Timing
 // logs the message, and the time needed to complete the action
+const timingCLog = () => {
+  console.time("fetching data");
+  fetch("https://api.github.com/users/nichelicorn")
+    .then(data => data.json())
+    .then(data => {
+    console.timeEnd("fetching data");
+    console.log(data);
+  });
+};
 
 // Tables
-console.table(dogs); 
 // logs data in a table format, rather than arrays / objects
+const tableCLog = () => {
+  console.table(dogs); 
+};
 
 // 🪟 Console in the Window
-const visualConsole = console.log;
-
-function displayConsole() {
-  
-};
-
-
-codeToVisualize = {
-  ...dogs,
-  
-};
 
 // rethink this - set the output as an element
 // return an innerHTML that displays the code in a div
 
-// console.log = function() {
-//   const message = [].join.call(arguments, " ");
-//   $(".output").text(message);
-//   visualConsole.apply(console, arguments);
-// }
-// console.log("👻");
+window.addEventListener("click", logDomElement);
