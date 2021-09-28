f1102bcc2ecf152505990026193c0e522b0f580b
Nichele Dunn <the7yearitch@hotmail.com>
2021-09-28 14:10:27 -0600

    💅 Create styles file and add clues with hover state

--- 12 - Key Sequence Detection/index.js
+++ 12 - Key Sequence Detection/index.js
@@ -2,17 +2,26 @@
 const pressed = [];
 const secretCode =[ "ArrowUp", "ArrowUp", "ArrowDown", "ArrowDown", "ArrowLeft", "ArrowLeft", "ArrowRight", "ArrowRight", "b", "a" ];
 
+const clues = document.querySelectorAll(".clue");
+
 // 👂 Event listeners
 window.addEventListener("keyup", unicornifyPage);
+window.addEventListener("keydown", tabOverClues);
 
 // ⚙️ Functions
 function unicornifyPage(e) {
   // push key value into an array
   pressed.push(e.key);
+  // console.log("pressed key <>>>", e.key);
   // trim array to a certain length (match number of characters in the secret code)
   pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length);
   if(pressed.join("") === secretCode.join("")) {
     console.log("are you ready for some unicorns?! 🦄✨");
     cornify_add();
   }
+  console.log("pressed keys <>>>", pressed);
 };
+
+function tabOverClues(e) {
+  console.log(e.key);
+}
