54d0c66a496f8a1bec47a7db6e3f9864fadce9e8
Nichele Dunn <the7yearitch@hotmail.com>
2021-10-08 11:49:12 -0600

    ♻️  Refactor into a plant list

--- 15 - LocalStorage/README.md
+++ 15 - LocalStorage/README.md
@@ -9,8 +9,12 @@
 * Add a button to check all / uncheck all
 
 #### What did I learn?
-* 
+* Can use `.this.querySelector()` to select an element based on an event
+* ES6 includes a shorthand property that allows an object key / value pair of the same name to be listed as a single word (eg, a key value pair of `name: name` could simply be listed as `name`)
+* A `submit` event fires directly on a form element, not on the `<button>` or `<input type="submit">` element that may have actually been clicked / received user input
+  * A form that does not pass validation will prevent submission from occurring
 
 #### References
 * [JavaScript30](https://javascript30.com/)
-* 
+* [Click event on MDN](https://developer.mozilla.org/en-US/docs/Web/API/Element/click_event)
+* [Submit event on MDN](https://developer.mozilla.org/en-US/docs/Web/API/HTMLFormElement/submit_event)
--- 15 - LocalStorage/index.js
+++ 15 - LocalStorage/index.js
@@ -1,50 +1,39 @@
 // 🔎 Query selectors
-const addItems = document.querySelector('.add-items'); // the form to add items to the list
-const itemsList = document.querySelector('.plates'); // the list to be updated
-const items = JSON.parse(localStorage.getItem("items")) || []; // loads
+const addItems = document.querySelector(".add-items");
+const itemsList = document.getElementById("itemsList");
+const items = JSON.parse(localStorage.getItem("items")) || []; // sets value as localStorage, if available, or an empty array if there is no localStorage
 
 // ⚙️ Functions
 function addItem(e) {
-  e.preventDefault(); // stops the page from reloading when the form is submitted; this is a default behavior
-  // console.log("target", e.target);
-  const text = (this.querySelector("[name=item]")).value; //looks inside the form tag for something with a name attribute of item; narrows the search on the page to just the form in question
+  e.preventDefault();
+  const text = (this.querySelector("[name=item]")).value; //looks inside the form tag for something with a name attribute of item; narrows the search on the page to elements with the listed attribute
   const item = {
-    text, // an ES6 shorthand - if the key & value have the same name, can use this shorthand instead of listing out key: value
+    text, // an ES6 shorthand - if the key & value have the same name, can use this shorthand instead of listing out text: text / key: value
     done: false
-  }
-  console.log("item <>>>", item);
-  // add the item to the items array
-  items.push(item);
+  };
+  items.push(item);   // add the item to the items array
   populateList(items, itemsList);
-  // set items to localStorage
-  localStorage.setItem("items", JSON.stringify(items));
+  localStorage.setItem("items", JSON.stringify(items)); // set items to localStorage
   this.reset(); // resets the form
 }
 
-function populateList( plates = [], platesList ) {
- // needs the list of items to populate; argument creates an empty array as placeholder for data, and platesList is the 
- platesList.innerHTML = plates.map((plate, i) => {
+function populateList( things = [], thingsList ) {
+ // needs the list of items to populate; argument creates an empty array as placeholder for data, and thingsList is the 
+ thingsList.innerHTML = things.map((thing, i) => {
    return `
     <li>
-      <input type="checkbox" data-index=${i} id="item${i}" ${plate.done ? "checked" : ""} /> 
-      <label for="item${i}">${plate.text}</label>
+      <input type="checkbox" data-index=${i} id="item${i}" ${thing.done ? "checked" : ""} /> 
+      <label for="item${i}">${thing.text}</label>
     </li>
    `;
- }).join(""); // the .join creates one string, rather than an array of strings
- // currently, this re-renders the list each time; React / Angular solve these problems
+ }).join(""); // .join creates one string, rather than an array of strings
 }
 
 function toggleDone(e) {
-  // console.log("e <>>>", e); // clicks on the label and the input elements
-  // want to target 
-  if (!e.target.matches("input")) {
-    return; // skip this unless it's an input
-    // console.log("e target <>>>", e.target);
-  }
+  if (!e.target.matches("input")) return;  // skip this unless it's an input
   const el = e.target;
-  console.log("el.dataset.index <>>>", el.dataset.index);
   const index = el.dataset.index;
-  items[index].done = !items[index].done; // toggles the value of this Boolean value
+  items[index].done = !items[index].done;
   localStorage.setItem("items", JSON.stringify(items));
   populateList(items, itemsList);
 }
