d080651229ff65019d64f06dc04bf98855e9f9b2
Nichele Dunn <the7yearitch@hotmail.com>
2021-10-21 15:46:30 -0600

    ♻️  Delete commented-out code'

--- 20 - Speech Detection/index.js
+++ 20 - Speech Detection/index.js
@@ -26,25 +26,6 @@ function printSpeech(e) {
   console.log("🗣", transcript);
 };
 
-// recognition.addEventListener("result", e => {
-//   // console.log("event <>>>", e.results);
-//   const transcript = Array.from(e.results)
-//     .map(result => result[0])
-//     .map(result => result.transcript)
-//     .join("")
-
-//   p.textContent = transcript;
-//   if (e.results[0].isFinal) {
-//     p = document.createElement("p");
-//     words.appendChild(p);
-//   };
-
-//   if (transcript.includes("unicorn")) {
-//     console.log("🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄")
-//   }
-//   console.log("🗣", transcript);
-// });
-
 // 👂 Event listeners
 recognition.start();
 recognition.addEventListener("end", recognition.start);
