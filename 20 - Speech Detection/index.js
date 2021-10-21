013a4897e2222f4c8fa29605fec9b26e88e46a1d
Nichele Dunn <the7yearitch@hotmail.com>
2021-10-21 15:45:26 -0600

    ♻️  Refactor anon function into function printSpeech

--- 20 - Speech Detection/index.js
+++ 20 - Speech Detection/index.js
@@ -1,16 +1,14 @@
+// 🌐 Global variables
 window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
-
 const recognition = new SpeechRecognition();
-// set interim results
 recognition.interimResults = true; // populates as you are speaking
-
 // Create a break so that it will create a new paragraph each time speech ends
 let p = document.createElement("p");
 const words = document.querySelector(".words");
 words.appendChild(p); // will add each new p to the DOM as speech continues
 
-recognition.addEventListener("result", e => {
-  // console.log("event <>>>", e.results);
+// ⚙️ Functions
+function printSpeech(e) {
   const transcript = Array.from(e.results)
     .map(result => result[0])
     .map(result => result.transcript)
@@ -26,9 +24,28 @@ recognition.addEventListener("result", e => {
     console.log("🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄")
   }
   console.log("🗣", transcript);
-});
+};
 
-recognition.addEventListener("end", recognition.start);
+// recognition.addEventListener("result", e => {
+//   // console.log("event <>>>", e.results);
+//   const transcript = Array.from(e.results)
+//     .map(result => result[0])
+//     .map(result => result.transcript)
+//     .join("")
 
+//   p.textContent = transcript;
+//   if (e.results[0].isFinal) {
+//     p = document.createElement("p");
+//     words.appendChild(p);
+//   };
+
+//   if (transcript.includes("unicorn")) {
+//     console.log("🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄🦄")
+//   }
+//   console.log("🗣", transcript);
+// });
+
+// 👂 Event listeners
 recognition.start();
-
+recognition.addEventListener("end", recognition.start);
+recognition.addEventListener("result", printSpeech);
