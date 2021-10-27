ee76039c12e3c4da3181171c3433013ce700ae50
Nichele Dunn <the7yearitch@hotmail.com>
2021-10-27 15:02:54 -0600

    ♻️ Move JavaScript to separate file

---
+++ 22 - Follow Along Link Highlighter/index.js
@@ -0,0 +1,27 @@
+    // 👀👀👀👀👀👀👀👀👀👀👀👀👀👀👀
+
+    const triggers = document.querySelectorAll("a"); // get all the trigger, all links in this case
+    const highlight = document.createElement("span");
+
+    highlight.classList.add("highlight");
+    document.body.append(highlight);
+
+    function highlightLink() {
+      // console.log("highlight!", this);
+      const linkCoords = this.getBoundingClientRect();
+      console.log("linkCoords <>>>", linkCoords); // returns info about where on the page `this` actually lives
+
+      const coords = {
+        width: linkCoords.width,
+        height: linkCoords.height,
+        top: linkCoords.top + window.scrollY,
+        left: linkCoords.left + window.scrollX,
+      }
+
+      highlight.style.width = `${coords.width}px`;
+      highlight.style.height = `${coords.height}px`;
+      highlight.style.transform = `translate(${coords.left}px, ${coords.top}px`;
+    };
+
+    triggers.forEach(a => a.addEventListener("mouseenter", highlightLink));
+
