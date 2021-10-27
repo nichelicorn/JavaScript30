be00cd0630c87ecc61edfc80a61d4a2007add330
Nichele Dunn <the7yearitch@hotmail.com>
2021-10-27 16:08:22 -0600

    ✨ Update page content to The Raven

--- 22 - Follow Along Link Highlighter/README.md
+++ 22 - Follow Along Link Highlighter/README.md
@@ -1,6 +1,6 @@
 # JavaScript30 no. 21 - Geolocation
 
-<!-- []() -->
+[]()
 
 <!-- *This project requires a local server - download the repo to run locally. 🙃* -->
 <!-- *Then, `npm i` in the terminal to install the server.* -->
@@ -11,8 +11,15 @@
   * Highlight the link that the mouse is currently hovering over
 
 #### What did I learn?
-* 
+* The `Element.getBoundingClientRect()` method return a `DOMRect` object that provides information about the size of an element, and its position relative to the viewport
+  * Returns an object containing coordinates and sizing, in pixels
+  * Coordinates are in relation to the top-left of the viewport
+* `width` and `height` properties returned also include `padding` and `border-width` measurements
+
+* A `DOMRect` object is the smallest rectangle containing the entire element; the size and position of the elements wrapper
+  * Sizing and coordinates are read-only
 
 #### References
 * [JavaScript30](https://javascript30.com/)
-* 
+* [`Element.getBoundingClientRect()` on MDN](https://developer.mozilla.org/en-US/docs/Web/API/Element/getBoundingClientRect)
+* [`DOMRect` on MDN](https://developer.mozilla.org/en-US/docs/Web/API/DOMRect)
--- 22 - Follow Along Link Highlighter/index.js
+++ 22 - Follow Along Link Highlighter/index.js
@@ -1,27 +1,26 @@
-    // 👀👀👀👀👀👀👀👀👀👀👀👀👀👀👀
+// 🔎 Query selectors
+const triggers = document.querySelectorAll("a"); // get all the triggers, links in this case
+const highlight = document.createElement("span");
 
-    const triggers = document.querySelectorAll("a"); // get all the trigger, all links in this case
-    const highlight = document.createElement("span");
+// highlight.classList.add("highlight");
+// document.body.append(highlight);
 
-    highlight.classList.add("highlight");
-    document.body.append(highlight);
+function highlightLink() {
+  // console.log("highlight!", this); // logs the element triggering the function
+  const linkCoords = this.getBoundingClientRect();
+  console.log("linkCoords <>>>", linkCoords); // returns info about where on the page `this` actually lives
 
-    function highlightLink() {
-      // console.log("highlight!", this);
-      const linkCoords = this.getBoundingClientRect();
-      console.log("linkCoords <>>>", linkCoords); // returns info about where on the page `this` actually lives
+  const coords = {
+    width: linkCoords.width,
+    height: linkCoords.height,
+    top: linkCoords.top + window.scrollY,
+    left: linkCoords.left + window.scrollX,
+  }
 
-      const coords = {
-        width: linkCoords.width,
-        height: linkCoords.height,
-        top: linkCoords.top + window.scrollY,
-        left: linkCoords.left + window.scrollX,
-      }
+  highlight.style.width = `${coords.width}px`;
+  highlight.style.height = `${coords.height}px`;
+  highlight.style.transform = `translate(${coords.left}px, ${coords.top}px`;
+};
 
-      highlight.style.width = `${coords.width}px`;
-      highlight.style.height = `${coords.height}px`;
-      highlight.style.transform = `translate(${coords.left}px, ${coords.top}px`;
-    };
-
-    triggers.forEach(a => a.addEventListener("mouseenter", highlightLink));
+triggers.forEach(a => a.addEventListener("mouseenter", highlightLink));
 
