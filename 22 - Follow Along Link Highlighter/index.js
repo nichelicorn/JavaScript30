2d06d75ddf4367ac85fc623496e2f604e210433a
Nichele Dunn <the7yearitch@hotmail.com>
2021-10-28 12:01:13 -0600

    ♻️ Add notes to JS

--- 22 - Follow Along Link Highlighter/index.js
+++ 22 - Follow Along Link Highlighter/index.js
@@ -1,26 +1,26 @@
 // 🔎 Query selectors
 const triggers = document.querySelectorAll("a"); // get all the triggers, links in this case
 const highlight = document.createElement("span");
-
 highlight.classList.add("highlight");
 document.body.append(highlight);
 
+// ⚙️ Functions
 function highlightLink() {
   // console.log("highlight!", this); // logs the element triggering the function
   const linkCoords = this.getBoundingClientRect();
-  console.log("linkCoords <>>>", linkCoords); // returns info about where on the page `this` actually lives
+  // console.log("linkCoords <>>>", linkCoords); // returns info about where on the page `this` actually lives
 
   const coords = {
     width: linkCoords.width,
     height: linkCoords.height,
     top: linkCoords.top + window.scrollY,
     left: linkCoords.left + window.scrollX,
-  }
+  };
 
   highlight.style.width = `${coords.width}px`;
   highlight.style.height = `${coords.height}px`;
   highlight.style.transform = `translate(${coords.left}px, ${coords.top}px`;
 };
 
+// 👂 Event listeners
 triggers.forEach(a => a.addEventListener("mouseenter", highlightLink));
-
